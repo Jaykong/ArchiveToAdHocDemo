356797a8e2c6e786ee24f19c240792cf185e5f11
Jay Kong <jay.kong@ef.com>
2018-06-08 17:18:20 +0800

    Initial Commit

---
+++ ArchiveToAdHocDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchiveToAdHocDemo
+//
+//  Created by JayKong on 2018/6/8.
+//  Copyright © 2018 JayKong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
